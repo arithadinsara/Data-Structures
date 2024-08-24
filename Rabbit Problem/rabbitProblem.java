319bdd754141fdad802c2d2fa17b41f83058a06e
Aritha Dinsara <arithadinsara123@gmail.com>
2024-08-24 18:21:26 +0530

    Update rabbitProblem.java
    This is how the rabbit problem changes with fibunnaci

--- Rabbit Problem/rabbitProblem.java
+++ Rabbit Problem/rabbitProblem.java
@@ -1,1 +1,20 @@
+public class RabbitProblem {
+
+    // Recursive method to calculate the number of rabbits in month n
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int months = 10; // Number of months to calculate
+        System.out.println("Rabbit Problem: Number of pairs of rabbits after " + months + " months:");
+        
+        for (int i = 0; i <= months; i++) {
+            System.out.println("Month " + i + ": " + fibonacci(i) + " pair(s) of rabbits");
+        }
+    }
+}
 
